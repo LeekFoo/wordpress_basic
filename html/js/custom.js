c191acab5837412c8c3a63a271e4c447545b0928
leek_foo <negi.44193@gmail.com>
2020-02-08 23:08:04 +0900

    ファーストビューを変更、企業概要ページを追加

--- html/js/custom.js
+++ html/js/custom.js
@@ -1,18 +1,28 @@
-$(function(){
+$(function () {
     // スライダー初期化
     $('.top-slides').slick({
         autoplay: true,
-        dots: true,
+        // dots: true,
         fade: true,
+        autoplaySpeed: 4000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        pauseOnDotsHover: false,
+    });
+
+    // AOS初期化
+    AOS.init({
+        duration: 800,
+        once: true,
     });
 
     // トップへ戻るボタンのスムーススクロール
-    $('a[href^="#"]').click(function(){
+    $('a[href^="#"]').click(function () {
         var speed = 500;
-        var href= $(this).attr("href");
+        var href = $(this).attr("href");
         var target = $(href == "#" || href == "" ? 'html' : href);
         var position = target.offset().top;
-        $("html, body").animate({scrollTop:position}, speed, "swing");
+        $("html, body").animate({ scrollTop: position }, speed, "swing");
         return false;
     });
 
@@ -20,16 +30,16 @@ $(function(){
     const nav = $('nav');
 
     // トップへ戻るボタン、グロナビの表示設定
-    $(window).on('load scroll', function() {
-        if($(window).scrollTop() > 100) {
+    $(window).on('load scroll', function () {
+        if ($(window).scrollTop() > 100) {
             $('.top-btn').fadeIn();
         } else {
             $('.top-btn').fadeOut();
         }
 
         // サイトトップの場合
-        if($('body').hasClass('home')) {
-            if($(window).scrollTop() > 100) {
+        if ($('body').hasClass('home')) {
+            if ($(window).scrollTop() > 100) {
                 header.addClass('bg-white');
             } else {
                 header.removeClass('bg-white');
@@ -38,7 +48,7 @@ $(function(){
     });
 
     // ハンバーガーメニュー
-    $('.navToggle').click(function() {
+    $('.navToggle').click(function () {
         nav.toggleClass('active');
 
         if (nav.hasClass('active')) {
@@ -46,5 +56,21 @@ $(function(){
         } else {
             header.removeClass('active');
         }
+    });
+
+    // ファーストビュー
+    $(window).on('load scroll', function () {
+        const elem = $('.catch-inner');
+
+        elem.each(function () {
+            const isPlay = 'isPlay';
+            const elemOffset = $(this).offset().top;
+            const scrollPos = $(window).scrollTop();
+            const wh = $(window).height();
+
+            if (scrollPos > elemOffset - wh + (wh / 4)) {
+                $(this).addClass(isPlay);
+            }
+        });
     });
 });
